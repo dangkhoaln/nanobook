09f166599580d62645c8704521ec06c72f623d3a
dangkhoaln <lndangkhoa19@gmail.com>
2023-09-01 21:02:41 +0700

    Basic finish

--- script.js
+++ script.js
@@ -15,9 +15,15 @@ function addBookToLibrary(title, author, year, haveRead) {
     myLibrary.push(book);
 }
 
+function removeBook(i) {
+    myLibrary.splice(i, 1);
+}
+
 // Render
 
 function renderBookList() {
+    const bookListElement = document.querySelector(".booklist");
+    bookListElement.innerHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
         renderBook(i);
     }
@@ -35,6 +41,11 @@ function renderBook(i) {
     const bookNameElement = document.createElement('li');
     bookNameElement.className = 'book_name';
     bookNameElement.textContent = myLibrary[i].title;
+    // Remove button
+    const bookReadButton = document.createElement('button');
+    bookReadButton.className = 'book_read';
+    bookReadButton.textContent = myLibrary[i].haveRead ? 'Unread' : 'Read';
+    bookReadButton.addEventListener('click', readButtonHandler);
     // Book author
     const bookAuthorElement = document.createElement('li');
     bookAuthorElement.className = 'book_author';
@@ -44,15 +55,12 @@ function renderBook(i) {
     bookYearElement.className = 'book_year';
     bookYearElement.textContent = myLibrary[i].year;
     // Read button
-    const bookReadButton = document.createElement('button');
-    bookReadButton.className = 'book_read';
-    bookReadButton.textContent = myLibrary[i].haveRead ? 'Unread' : 'Read';
-    // Remove button
     const bookRemoveButton = document.createElement('button');
     bookRemoveButton.className = 'book_remove';
     bookRemoveButton.textContent = 'X';
+    bookRemoveButton.addEventListener('click', removeBookHandler);
     // Assemble
-    bookInfoElement.append(bookNameElement, bookAuthorElement, bookYearElement, bookReadButton, bookRemoveButton);
+    bookInfoElement.append(bookNameElement, bookRemoveButton, bookAuthorElement, bookYearElement, bookReadButton);
     bookElement.appendChild(bookInfoElement);
     bookListElement.appendChild(bookElement);
 }
@@ -73,14 +81,32 @@ function addBookHandler(event) {
     const author = document.querySelector('#author').value;
     const year = document.querySelector('#year').value;
     addBookToLibrary(title, author, year, false);
-    renderBook(myLibrary.length-1);
+    renderBook(myLibrary.length - 1);
     closeCreateDialog();
-} 
+}
+
+function removeBookHandler(event) {
+    const bookElement = event.target.parentElement.parentElement;
+    const i = bookElement.dataset.key;
+    removeBook(i);
+    renderBookList();
+}
+
+function readButtonHandler(event) {
+    const bookElement = event.target.parentElement.parentElement;
+    const i = bookElement.dataset.key;
+    toggleRead(i);
+    renderBookList();
+}
+
+function toggleRead(i) {
+    myLibrary[i].haveRead = !myLibrary[i].haveRead;
+}
 
 const createBookButton = document.querySelector(".create-book");
 createBookButton.addEventListener('click', openCreateDialog);
 const submitButton = document.querySelector(".create-dialog button");
 submitButton.addEventListener('click', addBookHandler);
-addBookToLibrary(1, 2, 3, false);
-addBookToLibrary(4, 5, 6, true);
+addBookToLibrary("Sputnik Sweetheart", "Haruki Murakami", 1999, false);
+addBookToLibrary("The Catcher in the Rye", "J. D. Salinger", 1951, true);
 renderBookList();
